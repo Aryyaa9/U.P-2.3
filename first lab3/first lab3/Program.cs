b35471f43d132051adef38ffae96d7298b98e8d9
Aryyaa9 <153473157+Aryyaa9@users.noreply.github.com>
2024-04-03 12:56:13 +0700

    Add files via upload

---
+++ first lab3/first lab3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Worker
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public double Rate { get; set; }
+    public int Days { get; set; }
+    
+    public Worker(string name, string surname, double rate, int days)
+    {
+        Name = name;
+        Surname = surname;
+        Rate = rate;
+        Days = days;
+    }
+    
+    public void GetSalary()
+    {
+        double salary = Rate * Days;
+        Console.WriteLine($"Зарплата работника {Name} {Surname} за {Days} дней работы составляет: {salary}.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите имя:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Введите фамилию:");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine("Введите ставку за день работы:");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите количество отработанных дней:");
+        int days = Convert.ToInt32(Console.ReadLine());
+
+        
+        Worker worker = new Worker(name, surname, rate, days);
+        
+        worker.GetSalary();
+    }
+}
